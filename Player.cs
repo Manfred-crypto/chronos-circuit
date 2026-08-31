deadd203148500e5f121f79f042e9c09b75c5f89
Manfred-crypto <leungcy314@gmail.com>
2026-08-31 09:20:48 +0800

    fix: gravity

--- Player.cs
+++ Player.cs
@@ -1,17 +1,22 @@
 using Godot;
+
 public partial class Player : CharacterBody2D{
 	[Export]
 	public float Speed { get; set; } = 300.0f;
 	public override void _PhysicsProcess(double delta){
 		Vector2 velocity = Velocity;
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (!IsOnFloor()){
+			velocity.Y += 980.0f * (float)delta;
+		}
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()){
+			velocity.Y = -400.0f;
+		}
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "", "");
 		if (direction != Vector2.Zero){
 			velocity.X = direction.X * Speed;
-			velocity.Y = direction.Y * Speed;
 		}
 		else{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
 		Velocity = velocity;
 		MoveAndSlide();
