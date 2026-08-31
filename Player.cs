dead74f4f86a33ed6be7d2965574fabc47dfc998
Manfred-crypto <leungcy314@gmail.com>
2026-08-31 08:41:22 +0800

    fix: make code shorter

--- Player.cs
+++ Player.cs
@@ -1,36 +1,19 @@
 using Godot;
-using System.Collections.Generic;
 public partial class Player : CharacterBody2D{
-	[Export] private float Speed=300.0f;
-	[Export] private float JumpVelocity=-400.0f;
-	private struct FrameInput{
-		public float MoveDirection;
-		public bool JumpPressed;
-	}
-	private List<FrameInput> _recordedInputs=new List<FrameInput>();
-	private bool _isRecording=true;
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity=Velocity;
-		if (!IsOnFloor()){
-			velocity+= GetGravity() * (float)delta;
-		}
-		bool jumpPressed=false;
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()){
-			velocity.Y=JumpVelocity;
-			jumpPressed=true;
-		}
-		float direction=Input.GetAxis("ui_left", "ui_right");
-		if (direction != 0){
-			velocity.X=direction * Speed;
+	[Export]
+	public float Speed { get; set; } = 300.0f;
+	public override void _PhysicsProcess(double delta){
+		Vector2 velocity = Velocity;
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero){
+			velocity.X = direction.X * Speed;
+			velocity.Y = direction.Y * Speed;
 		}
 		else{
-			velocity.X=Mathf.MoveToward(Velocity.X, 0, Speed * 0.2f);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
-		Velocity=velocity;
+		Velocity = velocity;
 		MoveAndSlide();
-		if (_isRecording){
-			_recordedInputs.Add(new FrameInput { MoveDirection=direction, JumpPressed=jumpPressed });
-		}
 	}
 }
